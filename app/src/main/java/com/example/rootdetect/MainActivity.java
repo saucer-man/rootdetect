a7b71a1e8cd11962c7d4312c4ac69822f2b34c04
saucer-man <gyq0929@gmail.com>
2023-02-03 19:13:54 +0800

    first commit

---
+++ app/src/main/java/com/example/rootdetect/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.rootdetect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.rootdetect.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'rootdetect' library on application startup.
+    static {
+        System.loadLibrary("rootdetect");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'rootdetect' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
